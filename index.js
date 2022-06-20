6c64a13c9ef5496c48d84472ac4fc74e82c0d2c1
arina <awwwrina@gmail.com>
2022-06-20 20:48:30 +0300

    changes

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const app = express();
 app.use(express.json({ extended: true }));
 
 /* Возврат заголовков, для кроссдоменного AJAX (только для дев режима) */
-if (process.env.NODE_ENV !== 'production') {
+// if (process.env.NODE_ENV !== 'production') {
+    if (true) {
     app.use((req, res, next) => {
+
         res.header(`Access-Control-Allow-Origin`, '*');
         res.header(`Access-Control-Allow-Methods`, `GET, POST, OPTIONS, PUT, PATCH, DELETE`);
         res.header('Access-Control-Allow-Credentials', 'true');
